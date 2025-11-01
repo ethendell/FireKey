9147bcd46b520fe418399ad7a18fffc9c4c56be8
ethendell <127983879+ethendell@users.noreply.github.com>
2025-11-01 15:00:56 -0600

    Add keyword post-processing utilities and tests

---
+++ firekey/__init__.py
@@ -0,0 +1,13 @@
+"""FireKey metadata utilities."""
+
+from .keyword_processing import (
+    KeywordProcessingResult,
+    append_keyword_columns,
+    clean_keywords,
+)
+
+__all__ = [
+    "KeywordProcessingResult",
+    "append_keyword_columns",
+    "clean_keywords",
+]
